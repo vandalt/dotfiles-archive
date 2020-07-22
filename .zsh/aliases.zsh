fddcc53de5ca163caeefd2e545b52db1bb90bd09
Thomas Vandal <vandal@astro.umontreal.ca>
2020-07-22 00:53:44 -0400

    add colors to ls

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,14 @@
+# enable colors in ls
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
 # Use neovim
 alias v='nvim'
 alias vim='nvim'
