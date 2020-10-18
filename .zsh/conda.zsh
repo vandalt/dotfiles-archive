8e764a03cf540e9865c19d5173602fb0733647cc
Thomas Vandal <thomasvandal@hotmail.com>
2020-10-17 20:58:04 -0400

    update conda management

--- .zsh/conda.zsh
+++ .zsh/conda.zsh
@@ -5,52 +5,50 @@ else
     export PATH="$HOME/miniconda3/bin:$PATH"
 fi
 
-# Env var for mpi
-# OMPI_MCA_opal_cuda_support=true
-
-# Adapted from https://github.com/dfm/dotfiles and https://github.com/leouieda/dotfiles
+# Inspired from https://github.com/dfm/dotfiles and https://github.com/leouieda/dotfiles and
 cenv() {
 read -r -d '' CENV_HELP <<-'EOF'
 Usage: cenv [ACTION] [ARGS] [PACKAGES]
 Actions:
-  None         Creates or activates an environment
-  new|create   Creates a new environment
-  save         Saves existing environment info in a dir
-  up|update    Update the environment
+  None            Creates or activates an environment
+  new|create      Creates a new environment
+  up|update       Update the environment
   upall|update-all
-               Update all packages inside envionment to most recent version.
-               This updates both conda and pip packages
-  activate     Activates the environment
-  off|deactivate   Equivalent to 'conda deactivate'
-  add|install  Install a package
+                  Update all packages inside envionment to most recent version.
+                  This updates both conda and pip packages
+  on|activate     Activates the environment
+  off|deactivate  Equivalent to 'conda deactivate'
+  add|install     Install a package
   rm|remove|uninstall
-               Uninstall a package
-  dev|develop  Add development path to env (using conda.pth)
-  clean        Remove the environment
+                  Uninstall a package
+  dev|develop     Add development path to env (using conda.pth)
+  clean           Remove the environment
+  info            Show env info (conda info --envs output) and exit
 Arguments:
-  -h|--help    Show this message
-  -n|--name    Name of the envionment
-  -d|--dir     Directory where env info is saved (yml, lock and req files)
-               Note: This is not the environment prefix
-  -f|--file    The conda environment file
-  --pip        Install or uninstall a package with pip
+  -h|--help       Show this message
+  -n|--name       Name of the envionment
+                  Default: active
+  -d|--dir        Directory where env info is saved (yml, lock and req files)
+                  Note: This is NOT the environment prefix
+                  Default: ./env
+  -f|--file       The conda environment file (in dir)
+                  Default: environment.yml
+  -l|--lock       The conda lock file (in dir)
+                  Default: environment.lock
+  --pip           Install or uninstall a package with pip
 Packages: Packages to install
 EOF
 
   condaVersion=`conda --version`
   echo "$condaVersion"
 
-  # Find the action
+  # Find action
   case "$1" in
   new|create)
     action="create"
     shift
     ;;
-  save)
-    action="save"
-    shift
-    ;;
-  activate)
+  on|activate)
     action="activate"
     shift
     ;;
@@ -68,6 +66,10 @@ EOF
     ;;
   dev|develop)
     action="dev"
+    shift
+    ;;
+  info)
+    action="info"
     shift
     ;;
   up|update)
@@ -91,14 +93,12 @@ EOF
     return 0
   fi
 
+  envDir="./env"
   help=n
   pip=n
-  envName="$CONDA_DEFAULT_ENV"
-  echo $envName
-  active=y
-  if [[ -z $envName ]]; then
-    active=n
-  fi
+
+  # Get active environment, if any
+  current="$CONDA_DEFAULT_ENV"
 
   # now enjoy the options in order and nicely split until we see --
   while true; do
@@ -116,7 +116,7 @@ EOF
         shift 2
         ;;
       -d|--dir)
-        envName="$2"
+        envDir="$2"
         shift 2
         ;;
       -f|--file)
@@ -138,176 +138,191 @@ EOF
     return 0
   fi
 
-  # Check got at least some info about the env
-  if [[ -z "$envFile" && -z "$envName" ]]; then
-    envName="base"
+  if [[ "$action" = "info" ]]; then
+    conda info --envs
+    return 0
   fi
 
-  # Set env name
-  if [[ -z $envName ]]; then
-    envName=`sed -n -e 's/^name: //p' "$envFile"`
-  fi
-
-  # Set env dir
-  if [[ -z $envDir ]]; then
-    envDir="./env"
-  fi
-  
-  # Set env file
-  if [[ -z $envFile ]]; then
-    envFile="$envDir/environment.yml"
-  fi
-
-  # Default lock and pip files
-  lockFile="$envDir/environment.lock"
-  pipFile="$envDir/requirements.txt"
-
-  fileExists=n
-  if [[ -e "$envFile" ]]; then
-    fileExists=y
-  fi
-  envExists=n
-  if conda info --envs | grep -q "^$envName"; then
-    envExists=y
-  fi
+  # Make sure dir exists
   if [[ ! -d "$envDir" ]]; then
     mkdir -p $envDir
   fi
 
-  if [[ "$action" = "clean" ]]; then
-    read "flag?Remove this environment ($envName)? [y/N]: "
-    if [[ "$flag" = "y" ]] || [[ "$flag" = "Y" ]]; then
-      echo -n "Removing: environment $envName and dir $envDir"
-      conda deactivate
-      rm -rf $envDir
-      conda remove --name $envName --all
-    fi
-    return 0
+  # Assign env files
+  if [[ -z "$envFile" ]]; then
+    envFile="$envDir/environment.yml"
+  fi
+  if [[ -z "$lockFile" ]]; then
+    lockFile="$envDir/environment.lock"
+  fi
+  pipFile="$envDir/requirements.txt"
+
+  # Check we have a file
+  fileExists=n
+  if [[ -f "$envFile" ]]; then
+    fileExists=y
   fi
 
-  if [[ "$action" = "save" ]]; then
-    if [[ $envExists = "n" ]]; then
-      echo "Environment $envName does not exist"
-      return 1
+  # Make sure we have an env name
+  if [[ -z "$envName" ]]; then
+    if [[ ! -z "$current" ]]; then
+      envName="$current"
+    elif [[ "$fileExists" = "y" ]]; then
+      envName=`sed -n -e 's/^name: //p' "$envFile"`
     fi
-    conda list --export | grep pypi | cut -d = -f 1 >> $pipFile
-    echo "name: $envName" > $envFile
-    conda env export --name $envName --from-history | sed '1d;/^$/d' | sed '$d' >> $envFile
-    if [[ -e "$pipFile" ]]; then
-      echo "  - pip:" >> $envFile
-      echo "    - -r requirements.txt" >> $envFile
-    fi
-    conda env export --name $envName > $lockFile
-    return 0
+  fi
+  if [[ -z "$envName" ]]; then
+    # If still no env name, we have a problem
+    echo "No env name found. Either run this from an active environment, specify --name, or make sure that the env file contains a name"
+    return 1
   fi
 
-  if [[ $fileExists = "n" ]]; then
+  # Check if target env is active
+  active=n
+  if [[ "$current" = "$envName" ]]; then
+    active=y
+  fi
+
+  # Check the env exists
+  envExists=n
+  if conda info --envs | grep -q "^$envName"; then
+    envExists=y
+  fi
+
+  if [[ $envExists = "n" ]]; then
     if [[ "$action" = "activate" ]] || [[ "$action" = "update" ]]; then
       action="create"
     elif [[ "$action" != "create" ]]; then
-      echo "Environment file not found:" $envFile;
+      echo "Environment $envName not found";
       return 1
+    fi
+  fi
+
+  # If clean, just remove
+  if [[ "$action" = "clean" ]]; then
+    read "flag?Remove this environment ($envName)? [y/N]: "
+    if [[ "$flag" = "y" ]] || [[ "$flag" = "Y" ]]; then
+      echo -n "Removing: $lockFile $prefix"
+      # make sure no env active
+      conda deactivate
+      rm -rf "$lockFile $pipFile" 
+      conda remove --name $envName --all
+      return 0
+    elif [[ "$flag" = "n" ]] || [[ "$flag" = "N" ]]; then
+      echo "Aborting."
+      return 0
+    else
+      "Answer must be y or n, exiting"
+      return 2
     fi
   fi
 
   if [[ "$action" = "activate" ]]; then
     if [[ $envExists = "n" ]]; then
       echo "Environment $envName not found"
-      return 1
+      return 3
     fi
-    echo "Activating environment: $envName"
+    echo "Activating environment $envName"
     conda activate $envName
     return 0
   fi
 
-  if [[ "$action" = "create" ]]; then
-    if [[ $envExists = "y" ]]; then
-      echo "Environment $envName already exists"
-      return 1
-    fi
-    if [[ $fileExists = "y" ]]; then
-      echo "Creating a new environment from: $envFile"
-      conda env create --file $envFile
-    else
-      echo "Creating a new environment with arguments: $@"
-      conda create --name $envName pip $@
-    fi
-    conda activate $envName
-  elif [[ "$action" = "update" ]]; then
-    echo "Updating environment: $envFile -> $envName"
-    conda env update --name $envName -f $envFile --prune
-  elif [[ "$action" = "upall" ]]; then
-    conda activate $envName
-    conda clean -pity
-    conda update --all
-    conda list --export | grep pypi | cut -d = -f 1 >> $pipFile
-    conda list --export | grep pypi | cut -d = -f 1 | xargs -n1 pip install -U
+  if [[ "$action" = "dev" ]]; then
     if [[ "$active" = "n" ]]; then
-      conda deactivate
+      conda activate $envName
     fi
-  elif [[ "$action" = "dev" ]]; then
-    conda activate $envName
     conda develop $@
     if [[ "$active" = "n" ]]; then
       conda deactivate
     fi
+  fi
+
+
+  # All these other actions modify the env, so we use an if statement because we need to save the config at the very end
+  if [[ "$action" = "create" ]]; then
+    if [[ "$envExists" = "y" ]]; then
+      echo "Environment $envName already exists"
+      return 4
+    fi
+    if [[ $fileExists = "y" ]]; then
+      echo "Creating a new environment from: $envFile with name $envName"
+      conda env create --file $envFile --name $envName
+    else
+      echo "Creating a new environment with arguments: $@"
+      conda create --name $envName python pip $@
+    fi
+    conda activate $envName
+
+  elif [[ "$action" = "update" ]]; then
+    echo "Updating environment: $envFile -> $envName"
+    conda env update --name $envName -f $envFile --prune
+
   elif [[ "$action" = "add" ]]; then
     if [[ "$pip" = "y" ]]; then
+      if [[ "$active" = "n" ]]; then
+        conda activate $envName
+      fi
       echo "Installing (using pip): $@"
-      conda activate $envName
-      pip install $@
+      python -m pip install $@
       for dep in $@
       do
         echo $dep >> $pipFile
       done
       if [[ "$active" = "n" ]]; then
-        conda deactivate
+        conda deactivate $envName
       fi
     else
       echo "Installing: $@"
       conda install --name $envName $@
     fi
+
   elif [[ "$action" = "remove" ]]; then
     if [[ "$pip" = "y" ]]; then
-      echo "Uninstalling (using pip): $@"
-      conda activate $envName
-      pip uninstall $@
+      if [[ "$active" = "n" ]]; then
+        conda activate $envName
+      fi
+      echo "Installing (using pip): $@"
+      python -m pip uninstall $@
       for dep in $@
       do
         sed '/^$@/ d' $pipFile > $pipFile
       done
       if [[ "$active" = "n" ]]; then
-        conda deactivate
+        conda deactivate $envName
       fi
     else
-      echo "Uninstalling: $@"
-      conda remove --name $envName $@
+      echo "Installing: $@"
+      conda remove--name $envName $@
     fi
   fi
 
-  echo "name: $envName" > $envFile
-  conda env export --name $envName --from-history | sed '1d;/^$/d' | sed '$d' >> $envFile
-  if [[ -e "$pipFile" ]]; then
+  # Update config
+  conda env export --name $envName --from-history > $envFile
+  if [[ -f "$pipFile" ]]; then
     echo "  - pip:" >> $envFile
     echo "    - -r requirements.txt" >> $envFile
   fi
   conda env export --name $envName > $lockFile
-
 }
 
-cd_env () {
-    # Change directory and activate a conda environment if it's present.
-    # Use command to ignore the aliasing of cd
-    command cd $1;
-    if [[ -e "env/environment.yml" ]]; then
-        cenv;
-    else
-        cenv $HOME/environment.yml;
-    fi
+cdenv() {
+  # Function to cd in env and activate (take name from file)
+  # Must have env file in ./env/environment.yml
+  cd $1;
+  con
 }
 
-# other useful aliases
-alias cup='conda clean -pity; conda update --all'
-alias castro='conda activate astro'
-condastro='conda activate astro'
-alias cbase='conda activate base'
+coff() {
+  # Deativate current env
+  conda deactivate
+}
+
+con() {
+  # Function to activate env of project (take name from file)
+  envName=`sed -n -e 's/^name: //p' ./env/environment.yml`
+  conda activate $envName
+}
+
+# Useful aliases related to conda
+alias coff='conda deactivate'
+alias cinenv='conda info --envs'
